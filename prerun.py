3a357ea3ab6615b2d9618e1f47243c0070b31049
bellalongo <islo9452@colorado.edu>
2024-06-04 16:41:14 -0600

    she shows correct and all prerun plts now

--- prerun.py
+++ prerun.py
@@ -25,6 +25,10 @@ lightcurve_effects = ['Transits', 'Ellipsoidal', 'Radiation', 'Doppler beaming']
                 None
 """
 def preload_plots(df, cadence):
+    # Remove star data if it already exists
+    if exists(star_data_filename):
+        os.remove(star_data_filename)
+
     # Iterate through all rows with an orbital period
     for _, row in tqdm(df.iterrows(), desc="Processing lightcurves", total = len(df)):
         # Pull data for that star
@@ -36,7 +40,7 @@ def preload_plots(df, cadence):
             continue
     
         lightcurve = append_lightcurves(result, result_exposures, cadence)
-        if not lightcurve: continue # check if there was a result with the cadence needed
+        if not lightcurve: continue 
 
         # Star data
         star_name = 'TIC ' + str(lightcurve.meta['TICID'])
@@ -65,12 +69,12 @@ def preload_plots(df, cadence):
 
         # Save effects plots
         for i, effect in enumerate(lightcurve_effects):
-                effects_selection_plot(effect, lightcurve, binned_lightcurve, sine_fit, residuals, star_name)
-                plt.savefig('preload/' + folders[i] + '/' + star_name + '.png')
-                plt.clf()
+            effects_selection_plot(effect, lightcurve, binned_lightcurve, sine_fit, residuals, star_name)
+            plt.savefig('preload/' + folders[i] + '/' + star_name + '.png')
+            plt.clf()
 
         # Save star data to the csv
-        row = {"Star" : star_name, "Orbital Period(days)" : best_period}
+        row = {'Star' : star_name, 'Orbital Period(days)' : best_period, 'i Magnitude': star_imag}
         append_to_csv(star_data_filename, row)
 
 
@@ -84,10 +88,10 @@ def preload_plots(df, cadence):
 """
 def load_plots():
     # Load all plot images in the directory
-    images = [i for i in os.listdir('preload/plots')]
+    images = [i for i in os.listdir('preload/period_plots')]
 
     # Load dataframe
-    stars_df = pd.read_csv('preload/star_data.csv')
+    stars_df = pd.read_csv(star_data_filename)
 
     # Check if orbital period csv exists
     porb_filename = 'orbital_periods/periods.csv'
@@ -95,23 +99,43 @@ def load_plots():
         os.remove(porb_filename)
 
     # Iterate through all the images
-    for i, image in enumerate(images): # MAKE SURE CORRECT STAR DATA AND THAT TIC'S FOR IMAGE AND ROW MATCH
-        current_image = os.path.join('preload/plots', image)
-        image_name, _ = os.path.splitext(image)
+    for image in images: # MAKE SURE CORRECT STAR DATA AND THAT TIC'S FOR IMAGE AND ROW MATCH -> get csv data w/ tic
+        current_image = os.path.join('preload/period_plots/', image)
+        star_name, _ = os.path.splitext(image)
 
-        # Show the image
+        # Show period select plot
         fig = plt.figure(figsize=(14, 8))
-        cid = fig.canvas.mpl_connect('key_press_event', lambda event: on_key(event, 'Real period')) # MAKE SURE I WORK
+        cid = fig.canvas.mpl_connect('key_press_event', lambda event: on_key(event, 'Real period')) 
         plt.axis('off')
         plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
-
         img = mpimg.imread(current_image)
         plt.imshow(img)
         plt.show()
 
-        # Check if it was marked as real
-        if period_bool_list[i]: # CHANGE DATA THAT IS SAVED HERE
-            star_name = stars_df.iloc[i]['Star']
-            period = stars_df.iloc[i]['Orbital Period(days)']
-            row = {"Star" : star_name, "Orbital Period(days)" : period}
-            append_to_csv(porb_filename, row)
+        # Go to next plot if clicked 'no'
+        if not period_bool_list[len(period_bool_list) - 1]: continue
+
+        # Show each effects plot
+        for i, effect in enumerate(lightcurve_effects):
+            effect_filename = 'preload/' + folders[i] + '/' + star_name + '.png'
+            fig = plt.figure(figsize=(14, 8))
+            cid = fig.canvas.mpl_connect('key_press_event', lambda event: on_key(event, effect)) 
+            plt.axis('off')
+            plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+            img = mpimg.imread(effect_filename)
+            plt.imshow(img)
+            plt.show()
+        
+        # Get star data from csv
+        star_row = stars_df.loc[stars_df['Star'] == star_name]
+        
+        # Save data to csv
+        curr_index = len(doppler_beaming_bool_list) - 1
+        row = {'Star' : star_name, 
+                'Orbital Period(days)' : star_row['Orbital Period(days)'].values[0],
+                'i Magnitude': star_row['i Magnitude'].values[0],
+                'Transits': transits_bool_list[curr_index],
+                'Ellipsoidal': ellipsoidal_bool_list[curr_index],
+                'Radiation': radiation_bool_list[curr_index],
+                'Doppler beaming': doppler_beaming_bool_list[curr_index]} # will only hit this line if the period is real
+        append_to_csv(porb_filename, row)
