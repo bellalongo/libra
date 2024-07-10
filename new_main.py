1cb88afcb6117dc808e120a325693570e29c8e22
bellalongo <islo9452@colorado.edu>
2024-07-10 13:45:10 -0600

    making classes for fxns

---
+++ new_main.py
@@ -0,0 +1,27 @@
+from input_check import *
+from catalog_data import *
+
+def new_main():
+
+    # Catalog data 
+    raw_catalog_dir = 'raw_wdss_data.csv' # Raw query data from https://sdss-wdms.org/ 
+    catalog_dir = 'wdss_data.csv' # Query data w/ commas (file shouldn't exist on first run)
+    porb_dir = 'orbital_periods/periods.csv' # Where final orbital periods will be stored
+
+    # Lightcurve data
+    cadence = 120 # Desired cadence for lightcurves
+
+    # Choose how to run
+    preload = True # True if want to save all plots now, and look through them later
+    autopilot = False # True if want to just use a CNN to find periods
+
+    # Check inputs
+    InputCheck(raw_catalog_dir, catalog_dir, porb_dir, preload, autopilot)
+
+    # Process catalog data
+    catalog = CatalogData(raw_catalog_dir, catalog_dir, porb_dir)
+
+    # Make 
+
+if __name__ == '__main__':
+    new_main()
